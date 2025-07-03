92cc7f1738b27b8c786dbcbc252213b645bdbe64
Jayanth Devaraj Gowda <jayanthdevarajgowda@gmail.com>
2025-07-03 15:33:08 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import io
 import os
